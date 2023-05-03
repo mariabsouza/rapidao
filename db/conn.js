2d3694fd8b5b978f608e0b263e729cec07d3f97a
mariabsouza <maria.biasouza@gmail.com>
2023-05-03 19:56:58 -0300

    db config

--- db/conn.js
+++ db/conn.js
@@ -1,25 +1,17 @@
 const {Sequelize} = require('sequelize')
 
 
-// const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
-//   host: process.env.host,
-//   dialect: 'mysql',
-//   dialectOptions: {
-//     ssl: {
-//       rejectUnauthorized: true,
-//     },
-//   }
-// })
-
-const sequelize = new Sequelize('rapidao', 'on4ztacl14pv3bwdtj3o', 'pscale_pw_wY67DKXRZP77hm9OLWi1QWA5fAXbuQ4dC35lBQ577fD', {
-  host: 'aws.connect.psdb.cloud',
+const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
+  host: process.env.host,
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
       rejectUnauthorized: true,
     },
-  },
+  }
 })
+
+
 
 try {
   sequelize.authenticate()
