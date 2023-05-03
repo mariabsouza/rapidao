fcfc36cfa1e7be7a29874ff650f01dad2448d2db
mariabsouza <maria.biasouza@gmail.com>
2023-05-03 12:44:31 -0300

    db config

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,12 @@ const {Sequelize} = require('sequelize')
 
 const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
   host: process.env.host,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  }
 })
 
 try {
