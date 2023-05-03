ef037f01584638874f2c091c09e914c3256e47e9
mariabsouza <maria.biasouza@gmail.com>
2023-05-03 12:30:53 -0300

    database

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,6 @@
 const {Sequelize} = require('sequelize')
 
 
-
 const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
   host: process.env.host,
   dialect: 'mysql'
